9c337d510e3894ae05e00cc931cd69d5dfdbbb33
Greg Stickel <gregory.stickel@live.com>
2022-10-17 10:02:20 -0400

    Final commit

---
+++ buildTower.js
@@ -0,0 +1,14 @@
+function buildTower (floors) {
+    let tower = [];
+    for (let i = 0; i < floors; i++) {
+        tower.push(" ".repeat(floors-i-1)+
+        "*".repeat((i*2)+1)+
+        " ".repeat(floors-i-1));
+
+    }
+
+    return tower;
+}
+
+
+console.log(buildTower(10));
